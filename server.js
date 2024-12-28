28076d111fc5f76ca6c6b9e226477abf04ed9229
Arthurr <arthurrwith2r@gmail.com>
2024-12-28 22:35:35 +0700

    Update Render pt.1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 const mysql = require('mysql2');
 const secretKey = "my-portfolio";
 const jwt = require ('jsonwebtoken');
