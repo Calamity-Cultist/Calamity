22b843ca8b95c736087898aa01971c8f67c3b835
Arthurr <arthurrwith2r@gmail.com>
2024-12-30 00:38:16 +0700

    testing something

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5004;
 const mysql = require('mysql2');
 const secretKey = "my-portfolio";
 const jwt = require ('jsonwebtoken');
